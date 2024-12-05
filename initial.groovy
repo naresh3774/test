b18f6f1ace111f772b9a44ba306d225ebd08dc98
Naresh Sharma <naresh3774@outlook.com>
2024-12-05 12:26:07 -0500

    updated

--- initial.groovy
+++
@@ -1,9 +0,0 @@
-def environment = 'staging' // Change this to 'dev' or 'staging' as needed
-
-if (environment == 'dev') {
-    sh 'cp src/environments/environment.dev.ts src/environments/environment.ts'
-} else if (environment == 'staging') {
-    sh 'cp src/environments/environment.test.ts src/environments/environment.ts'
-} else {
-    echo "Unknown environment: ${environment}"
-}
