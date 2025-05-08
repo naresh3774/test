fc031440921cb859dda757b4ec677ca7d6e61afb
Naresh Sharma <naresh3774@outlook.com>
2025-05-08 12:16:49 -0400

    updated

--- rough/terraform/dsc_registration.tf
+++ rough/terraform/dsc_registration.tf
@@ -1,11 +1,18 @@
-# This is the actual null_resource using the local-exec provisioner:
-
 resource "null_resource" "register_dsc_nodes" {
   provisioner "local-exec" {
-    command = "pwsh ./scripts/register_all_dsc.ps1 -vmNames ${join(',', var.dsc_config.vm_names)} -vmRG ${var.dsc_config.vm_resource_group} -aan ${var.dsc_config.automation_account} -aarg ${var.dsc_config.automation_resource_rg}"
+    command = "pwsh ./scripts/register_all_dsc.ps1 -vmNames ${join(",", var.dsc_config.vm_names)} -vmRG ${var.dsc_config.vm_resource_group} -aan ${var.dsc_config.automation_account} -aarg ${var.dsc_config.automation_resource_rg}"
   }
 
-  depends_on = [
-    module.virtual_machines # Adjust if your module is named differently
-  ]
+  # Remove broken depends_on for now. Optionally wait on a known resource
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 }
+
+#  Optional: Safe Dependency Alternative
+
+# If you know the name of one actual resource being created by CAF (e.g., a public IP or NIC), you can depend on that:
+
+# depends_on = [
+#   azurerm_network_interface.example_vm2_nic0
+# ]
