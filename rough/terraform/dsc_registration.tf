998443821183bd9047a239d41a623e926ef979ed
Naresh Sharma <naresh3774@outlook.com>
2025-05-08 17:07:07 -0400

    updated

--- rough/terraform/dsc_registration.tf
+++ rough/terraform/dsc_registration.tf
@@ -2,14 +2,7 @@ resource "null_resource" "register_dsc_nodes" {
   count = length(var.dsc_config.vm_names)
 
   provisioner "local-exec" {
-    command = <<EOT
-az vm run-command invoke \
-  --command-id RunPowerShellScript \
-  --name ${element(var.dsc_config.vm_names, count.index)} \
-  --resource-group ${var.dsc_config.vm_resource_group} \
-  --scripts @scripts/register_dsc_remote.ps1 \
-  --parameters "AutomationAccountName=${var.dsc_config.automation_account}" "AutomationResourceGroup=${var.dsc_config.automation_resource_rg}" "NodeConfigurationName=Gis20CopyScripts.${element(var.dsc_config.vm_names, count.index)}"
-EOT
+    command = "az vm run-command invoke --command-id RunPowerShellScript --name ${element(var.dsc_config.vm_names, count.index)} --resource-group ${var.dsc_config.vm_resource_group} --scripts @scripts/register_dsc_remote.ps1 --parameters \"AutomationAccountName=${var.dsc_config.automation_account}\" \"AutomationResourceGroup=${var.dsc_config.automation_resource_rg}\" \"NodeConfigurationName=Gis20CopyScripts.${element(var.dsc_config.vm_names, count.index)}\""
   }
 
   triggers = {
