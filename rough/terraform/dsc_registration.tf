eb70e7f13c6d20a85c1da837749a9bcf666852c9
Naresh Sharma <naresh3774@outlook.com>
2025-05-09 17:02:32 -0400

    updated

--- rough/terraform/dsc_registration.tf
+++ rough/terraform/dsc_registration.tf
@@ -1,3 +1,4 @@
+# register_dsc_nodes
 resource "null_resource" "register_dsc_nodes" {
   count = length(var.dsc_config.vm_names)
 
@@ -27,3 +28,30 @@ resource "null_resource" "register_dsc_nodes" {
   }
 }
 
+# install_dsc_prerequisites
+resource "null_resource" "install_dsc_prerequisites" {
+  count = length(var.dsc_config.vm_names)
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      $clientId = az keyvault secret show --vault-name ${var.dsc_config.keyvault_name} --name client-id --query value -o tsv
+      $clientSecret = az keyvault secret show --vault-name ${var.dsc_config.keyvault_name} --name client-secret --query value -o tsv
+      $tenantId = az keyvault secret show --vault-name ${var.dsc_config.keyvault_name} --name tenant-id --query value -o tsv
+
+      az login --service-principal -u $clientId -p $clientSecret --tenant $tenantId
+
+      az vm run-command invoke `
+        --command-id RunPowerShellScript `
+        --name ${var.dsc_config.vm_names[count.index]} `
+        --resource-group ${var.dsc_config.vm_resource_group} `
+        --scripts @scripts/prerequisites.ps1 `
+        --parameters `
+          "VmName=${var.dsc_config.vm_names[count.index]}" `
+          "VmResourceGroup=${var.dsc_config.vm_resource_group}"
+    EOT
+
+    interpreter = ["PowerShell", "-Command"]
+  }
+
+  depends_on = [null_resource.register_dsc_nodes]
+}
