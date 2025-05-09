b8defec753b41b05196149ea1296e94287739aa7
Naresh Sharma <naresh3774@outlook.com>
2025-05-09 17:44:27 -0400

    moved

--- rough/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "dsc_config" {
-  type = object({
-    vm_names               = list(string)
-    vm_resource_group      = string
-    automation_account     = string
-    automation_resource_rg = string
-    keyvault_name          = string
-  })
-}
